48c186f20df429f1326f55753451f9286a3beb85
Bincy Narath <bincybaburajnarath@gmail.com>
2020-08-08 19:33:22 -0400

    Finished featured images

--- Code/urls_list.py
+++ Code/urls_list.py
@@ -1,3 +1,4 @@
 nasa_mars_news = 'https://mars.nasa.gov/news/'
+nasa_jpl = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
 executable_path = '/usr/local/bin/chromedriver'
 browser_choice = 'chrome'
